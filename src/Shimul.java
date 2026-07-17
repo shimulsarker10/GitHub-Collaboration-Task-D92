e414fbce51a51c4430170469bc96b9798778a3af
snsamim <shoaibnurainsamim@gmail.com>
2026-07-17 23:14:55 +0600

    add gatter and setter

--- src/Shimul.java
+++ src/Shimul.java
@@ -36,5 +36,22 @@ public class Shimul {
         this.age = age;
     }
 
+    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
 
 }
